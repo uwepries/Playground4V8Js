b5b10a22c45c6c8736e7e2a08d6efd7e996a0b89
Uwe Pries <uwe.pries@digartis.de>
2012-08-30 12:24:31 +0200

    bind v8, code styling, drop dependency

--- v8.php
+++ v8.php
@@ -2,11 +2,12 @@
 
 $mysqli = new mysqli("localhost", "bikini", "bikini", "bikini");
 
-V8Js::registerExtension('string', file_get_contents('string.js'), array('function'));
 V8Js::registerExtension('function', file_get_contents('function.js'));
+V8Js::registerExtension('string', file_get_contents('string.js'));
 
 $v8 = new V8Js('PHP', array(), array('function', 'string'));
-$v8->sprintf = function ($string) {
+
+$v8->sprintf = function($string) {
     return call_user_func_array('sprintf', func_get_args());
 };
 
@@ -25,8 +26,9 @@ $v8->query = function($query) use ($mysqli) {
     return $data;
 };
 
-function v8_exec($v8, $path) {
+$v8_exec = function($path) use ($v8) {
     return $v8->executeString(file_get_contents($path), basename($path));
-}
+};
 
-v8_exec($v8, 'main.js');
+$v8_exec('main.js');
+
